241fc263d4e6c9987b4175ecf1d7193449af52e3
Arodefarodef <umudefedora@gmail.com>
2024-06-23 12:30:57 -0700

    working on

---
+++ app/(screen)/register/page.tsx
@@ -0,0 +1,112 @@
+"use client";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import React, { useState } from "react";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+import { MdLock } from "react-icons/md";
+
+const Page = () => {
+  const [show, setShow] = useState(true);
+  const formAction = async (formData: FormData) => {
+    // "use server";
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    const url = "/api/user";
+
+    await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+    }).then(() => {
+      redirect("/signin");
+    });
+  };
+  return (
+    <div className="bg-gray-200 h-[100vh] flex justify-center items-center">
+      <form
+        action={formAction}
+        className="bg-white border p-10 border-gray-500 flex flex-col gap-4"
+      >
+        <div className="flex flex-col gap-2">
+          <h1 className="font-mono text-[20px] font-bold">
+            Registeration Form
+          </h1>
+          <div className="w-[30%] bg-green-300 h-[4px]"></div>
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <span className="font-sans font-light text-[13px]">Name</span>
+          <input
+            className="border border-gray-400 p-2 rounded-sm outline-none focus:shadow-md focus:border-blue-600 hover:border-black text-[12px] font-medium"
+            type="text"
+            name="name"
+          />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <span className="font-sans font-light text-[12px]">Email</span>
+          <input
+            className="border border-gray-400 p-2 rounded-sm outline-none focus:shadow-md focus:border-blue-600 hover:border-black text-[12px] font-medium"
+            type="text"
+            name="email"
+          />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <span className="font-sans font-light text-[12px]">Password</span>
+          <div className="flex justify-between items-center border border-gray-400  p-2 rounded-sm outline-non hover:border-black text-[12px] font-medium">
+            <input
+              className=" outline-none"
+              type={show ? "password" : "text"}
+              name="password"
+            />
+            <div
+              onClick={() => {
+                setShow(!show);
+              }}
+            >
+              {show ? <IoMdEyeOff /> : <IoMdEye />}
+            </div>
+          </div>
+
+          <Link
+            className="text-[10px] hover:underline text-blue-600"
+            href="/signin"
+          >
+            Already have an account? Sign in here
+          </Link>
+        </div>
+
+        <button className="bg-blue-600 p-2 rounded-[60px] text-white font-sans text-[]14px flex justify-center items-center gap-2 outline-none">
+          <div>
+            <MdLock />
+          </div>
+          <div>Register</div>
+        </button>
+        <div className="flex flex-col gap-1 justify-center items-center">
+          <Link
+            className="hover:underline font-sans font-light text-[13px] text-blue-600"
+            href=""
+          >
+            Forgot email?
+          </Link>
+          <Link
+            className="hover:underline font-sans font-light text-[13px] text-blue-600"
+            href=""
+          >
+            Forgot password?
+          </Link>
+          <Link
+            className="hover:underline font-sans font-light text-[13px] text-blue-600"
+            href=""
+          >
+            Need help?
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Page;
